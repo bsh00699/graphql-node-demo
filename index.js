9f8fb034dc47e936a8c5778c3e72afda9e333764
bsh00699 <525534397@qq.com>
2021-12-24 22:47:05 +0800

    feat: 添加graphql运行实例

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import Koa from 'koa'
 import Router from 'koa-router'
+import { graphqlHTTP } from 'koa-graphql'
+// import { buildSchema } from 'graphql'
+import schema from './src/schema'
 // const Koa = require('koa')
 // const Router = require('koa-router')
 
 const app = new Koa()
 const router = new Router()
+router.all('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+app.use(router.routes(), router.allowedMethods())
 
 const PORT = process.env.PORT || 3000
 
